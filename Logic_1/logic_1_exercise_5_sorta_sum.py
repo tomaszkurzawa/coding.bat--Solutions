4754291e1ed6619294783a13c975baeee4f0ac41
teekay <60406001+tomaszkurzawa@users.noreply.github.com>
2020-03-02 21:35:22 +0100

    Add files via upload

---
+++ Logic_1/logic_1_exercise_5_sorta_sum.py
@@ -0,0 +1,13 @@
+"""
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+
+
+sorta_sum(3, 4) → 7
+sorta_sum(9, 4) → 20
+sorta_sum(10, 11) → 21
+"""
+def sorta_sum(a, b):
+  result = a+b
+  if result >=10 and result <=19:
+    return 20
+  return result
